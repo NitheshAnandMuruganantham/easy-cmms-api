0441df0933fa4925df08eaef08594a4a9b107c3c
nithesh anand <anand252825@gmail.com>
2024-02-20 20:38:12 +0530

    fix: 🐛 fixed errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   providers: [
@@ -24,6 +27,15 @@ import { APP_GUARD } from '@nestjs/core';
         secret: configService.get('JWT_SECRET'),
         signOptions: { expiresIn: '10d' },
         global: true,
+      }),
+      inject: [ConfigService],
+    }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore as any,
+        url: configService.get('REDIS_URL'),
+        isGlobal: true,
       }),
       inject: [ConfigService],
     }),
